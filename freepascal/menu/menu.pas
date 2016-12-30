6de24e5e807a69fac0cd67137509eb0d138b3e82
lacerto <ladislaus@zoho.com>
2016-12-30 19:33:09 +0100

    Added example for the Menu task

---
+++ freepascal/menu/menu.pas
@@ -0,0 +1,44 @@
+program Menu;
+uses
+  objects;
+var
+  MenuItems :PUnSortedStrCollection;
+  selected  :string;
+
+Function SelectMenuItem(MenuItems :PUnSortedStrCollection):string;
+var
+  i, idx :integer;
+  code   :word;
+  choice :string;
+begin
+  repeat
+    for i:=0 to MenuItems^.Count-1 do
+    begin
+      writeln(i+1:2, ') ', PString(MenuItems^.At(i))^);
+    end;
+    write('Make your choice: ');
+    readln(choice);
+    // Try to convert choice to an integer.
+    // Code contains 0 if this was successful.
+    val(choice, idx, code)
+  until (code=0) and (idx>0) and (idx<=MenuItems^.Count);
+  // Return the selected element.
+  SelectMenuItem := PString(MenuItems^.At(idx-1))^;
+end;
+
+begin
+  // Create an unsorted string collection for the menu items.
+  MenuItems := new(PUnSortedStrCollection, Init(10, 10));
+
+  // Add some menu items to the collection.
+  MenuItems^.Insert(NewStr('fee fie'));
+  MenuItems^.Insert(NewStr('huff and puff'));
+  MenuItems^.Insert(NewStr('mirror mirror'));
+  MenuItems^.Insert(NewStr('tick tock'));
+
+  // Display the menu and get user input.
+  selected := SelectMenuItem(MenuItems);
+  writeln('You chose: ', selected);
+
+  dispose(MenuItems, Done);
+end.
